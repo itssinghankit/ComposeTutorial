6b837e3902f1cd890817e94565a532444670f34c
itssinghankit <singhankit.kr@gmail.com>
2023-09-29 18:51:09 +0530

    rows and columns

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -31,7 +38,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showSystemUi = true)
+@Composable
+fun Rows() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "A")
+        Text(text = "B")
+    }
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "C")
+        Text(text = "D")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+//@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Text() {
+    val state = remember { mutableStateOf("") }
+    TextField(
+        value = state.value,
+        onValueChange = {
+            state.value = it
+        },
+        label = { Text(text = "Name", color = Color.Blue) })
+}
+
+
 @Composable
 fun Btton() {
     Button(
@@ -41,7 +80,7 @@ fun Btton() {
             containerColor = Color.Gray
         ),
 
-    )
+        )
     {
         Text(text = "SUBMIT", fontSize = 24.sp)
     }
